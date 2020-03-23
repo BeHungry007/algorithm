06f7c4d9ce1183a6f3b143ea09536aad5b99c7eb
xxk <xinxk@foxmail.com>
2020-03-23 21:31:35 +0800

    27

---
+++ algorithm/src/main/java/juniorDemo/demo100/Demo27.java
@@ -0,0 +1,64 @@
+package juniorDemo.demo100;
+
+import org.junit.Test;
+
+//给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+//不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+//元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+public class Demo27 {
+    @Test
+    public void test1() {
+        int[] nums = {3,2,2,3};
+        removeElement(nums, 3);
+    }
+
+    public int removeElement(int[] nums, int val) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[a] = nums[i];
+                a++;
+            }
+        }
+        return a;
+
+    }
+
+    public int removeElement2(int[] nums, int val) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int n = nums.length;
+        while (count < n) {
+            if (nums[count] == val) {
+                nums[count] = nums[n - 1];
+                n--;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int removeElement1(int[] nums, int val) {
+        if( nums == null || nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                a++;
+                nums[i] = nums[nums.length - a];
+                i--;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
