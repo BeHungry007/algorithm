dd1ea088277679e2b948f50d6d0772e38d7229c1
ByHungry007 <xxk240704>
2020-04-22 20:13:41 +0800

    92 + 670

---
+++ algorithm/src/main/java/middleDemo/Demo100/Demo670.java
@@ -0,0 +1,39 @@
+package middleDemo.Demo100;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class Demo670 {
+    @Test
+    public void test01(){
+        maximumSwap(9973);
+    }
+    public int maximumSwap(int num) {
+        char[] A = Integer.toString(num).toCharArray();
+        String s = "" + num;
+        int[] arr = new int[10];
+        for (int i = 0; i < s.length(); i++) {
+            int a = s.charAt(i) - '0';
+            arr[a] = i;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 9; j > s.charAt(i) - '0' ; j--) {
+                if (arr[j] > i) {
+                    char tmp = A[i];
+                    A[i] = A[arr[j]];
+                    A[arr[j]] = tmp;
+                    return Integer.valueOf(new String(A));
+                }
+            }
+        }
+        return num;
+    }
+
+    @Test
+    public void test02(){
+        String s = "12345678";
+        String substring = s.substring(1, 2);
+        System.out.println(substring);
+    }
+}
---
+++ algorithm/src/main/java/middleDemo/Demo100/Demo92.java
@@ -0,0 +1,35 @@
+package middleDemo.Demo100;
+
+import org.junit.Test;
+
+/**
+ * 反转链表II
+ * 反转从位置m到n的链表。请使用一趟完成反转。
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+public class Demo92 {
+    @Test
+    public void test01(){
+
+    }
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode pre = new ListNode(-1);
+        pre.next = head;
+        ListNode pre1 = pre;
+        for (int i = 1; i < m; i++) {
+            pre1 = pre1.next;
+        }
+        ListNode ln = pre1.next;
+        for (int i = m; i < n; i++) {
+            ListNode next = ln.next;
+            ln.next = next.next;
+            next.next = pre1.next;
+            pre1.next = next;
+        }
+        return pre.next;
+    }
+}
